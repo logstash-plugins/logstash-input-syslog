2c04a82744d3996c79769fe0980a2e39d54a35ae
Colin Surprenant <colin.surprenant@gmail.com>
2015-01-07 15:38:22 -0500

    locale/timezone configs, concurrency issues, specs locale config comment and support for timezoneconfig
    thread safe shutdown flag, proper rescue block+shutdown handling
    
    added atomic gem dependency
    
    re-enabled all tests, added specs for locale and timezone
    
    switch from atomic to concurrent-ruby
    
    reorder requires
    
    refactored thread code, fixed shutdown logic, some cleanups
    
    shutdown pipeline before assertions
    
    explicit dependency on thread_safe
    
    non threadsafe array reinitialization bug
    
    refactoed decoding, added comments
    
    cleanups
    
    DRYing

--- spec/inputs/syslog_spec.rb
+++ spec/inputs/syslog_spec.rb
@@ -1,13 +1,14 @@
-# coding: utf-8
+# encoding: utf-8
 require "logstash/devutils/rspec/spec_helper"
-require "socket"
 require "logstash/inputs/syslog"
 require "logstash/event"
+require "stud/try"
+require "socket"
 
 describe "inputs/syslog" do
+  SYSLOG_LINE = "<164>Oct 26 15:19:25 1.2.3.4 %ASA-4-106023: Deny udp src DRAC:10.1.2.3/43434 dst outside:192.168.0.1/53 by access-group \"acl_drac\" [0x0, 0x0]"
 
-
-  it "should properly handle priority, severity and facilities", :socket => true do
+  describe "should properly handle priority, severity and facilities" do
     port = 5511
     event_count = 10
 
@@ -16,34 +17,38 @@ describe "inputs/syslog" do
         syslog {
           type => "blah"
           port => #{port}
-          locale => "en"
         }
       }
     CONFIG
 
     input do |pipeline, queue|
-      Thread.new { pipeline.run }
+      t = Thread.new { pipeline.run }
       sleep 0.1 while !pipeline.ready?
 
       socket = Stud.try(5.times) { TCPSocket.new("127.0.0.1", port) }
       event_count.times do |i|
-        socket.puts("<164>Oct 26 15:19:25 1.2.3.4 %ASA-4-106023: Deny udp src DRAC:10.1.2.3/43434 dst outside:192.168.0.1/53 by access-group \"acl_drac\" [0x0, 0x0]")
+        socket.puts(SYSLOG_LINE)
       end
       socket.close
 
       events = event_count.times.collect { queue.pop }
 
+      # important to shutdown here before any assertion so that the pipeline + socket
+      # cleanups are correctly done before any potential spec error that would result
+      # in aborting execution and not doing the cleanup.
+      pipeline.shutdown
+      t.join
+
       insist { events.length } == event_count
-      event_count.times do |i|
-        insist { events[i]["priority"] } == 164
-        insist { events[i]["severity"] } == 4
-        insist { events[i]["facility"] } == 20
-        insist { events[i]["@timestamp"] } == "2014-10-26T14:19:25.000Z"
+      events.each do |event|
+        insist { event["priority"] } == 164
+        insist { event["severity"] } == 4
+        insist { event["facility"] } == 20
       end
     end
   end
 
-  it "should add unique tag when grok parsing fails with live syslog input", :socket => true do
+  describe "should add unique tag when grok parsing fails with live syslog input" do
     port = 5511
     event_count = 10
 
@@ -57,7 +62,7 @@ describe "inputs/syslog" do
     CONFIG
 
     input do |pipeline, queue|
-      Thread.new { pipeline.run }
+      t = Thread.new { pipeline.run }
       sleep 0.1 while !pipeline.ready?
 
       socket = Stud.try(5.times) { TCPSocket.new("127.0.0.1", port) }
@@ -68,11 +73,101 @@ describe "inputs/syslog" do
 
       events = event_count.times.collect { queue.pop }
 
+      # important to shutdown here before any assertion so that the pipeline + socket
+      # cleanups are correctly done before any potential spec error that would result
+      # in aborting execution and not doing the cleanup.
+      pipeline.shutdown
+      t.join
+
       insist { events.length } == event_count
       event_count.times do |i|
-        insist { events[i]["tags"] } == ["_grokparsefailure_sysloginputplugin"]
+        insist { events[i]["tags"] } == ["_grokparsefailure_sysloginput"]
       end
     end
+  end
+
+  describe "should properly handle locale and timezone" do
+    port = 5511
+    event_count = 10
+
+    config <<-CONFIG
+      input {
+        syslog {
+          type => "blah"
+          port => #{port}
+          locale => "en"
+          timezone => "UTC"
+        }
+      }
+    CONFIG
+
+    input do |pipeline, queue|
+      t = Thread.new { pipeline.run }
+      sleep 0.1 while !pipeline.ready?
+
+      socket = Stud.try(5.times) { TCPSocket.new("127.0.0.1", port) }
+      event_count.times do |i|
+        socket.puts(SYSLOG_LINE)
+      end
+      socket.close
+
+      events = event_count.times.collect { queue.pop }
+
+      # important to shutdown here before any assertion so that the pipeline + socket
+      # cleanups are correctly done before any potential spec error that would result
+      # in aborting execution and not doing the cleanup.
+      pipeline.shutdown
+      t.join
+
+      insist { events.length } == event_count
+      events.each do |event|
+        insist { event["@timestamp"].to_iso8601 } == "#{Time.now.year}-10-26T15:19:25.000Z"
+      end
+    end
+  end
+
+  describe "should properly handle no locale and no timezone" do
+    port = 5511
+
+    config <<-CONFIG
+      input {
+        syslog {
+          type => "blah"
+          port => #{port}
+        }
+      }
+    CONFIG
+
+    input do |pipeline, queue|
+      t = Thread.new { pipeline.run }
+      sleep 0.1 while !pipeline.ready?
+
+      socket = Stud.try(5.times) { TCPSocket.new("127.0.0.1", port) }
+      socket.puts(SYSLOG_LINE)
+      socket.close
+
+      event = queue.pop
+
+      # important to shutdown here before any assertion so that the pipeline + socket
+      # cleanups are correctly done before any potential spec error that would result
+      # in aborting execution and not doing the cleanup.
+      pipeline.shutdown
+      t.join
+
+      # chances platform timezone is not UTC so ignore the hours
+      insist { event["@timestamp"].to_iso8601 } =~ /#{Time.now.year}-10-26T\d\d:19:25.000Z/
+    end
+  end
+
+  it "should support non UTC timezone" do
+    input = LogStash::Inputs::Syslog.new({"timezone" => "-05:00"})
+    input.register
+
+    # event which is not syslog should have a new tag
+
+    syslog_event = LogStash::Event.new({ "message" => "<164>Oct 26 15:19:25 1.2.3.4 %ASA-4-106023: Deny udp src DRAC:10.1.2.3/43434" })
+    input.syslog_relay(syslog_event)
+    insist { syslog_event["@timestamp"].to_iso8601 } == "#{Time.now.year}-10-26T20:19:25.000Z"
   end
 
   it "should add unique tag when grok parsing fails" do
